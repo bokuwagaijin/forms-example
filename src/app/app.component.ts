d8ed17a583e3bc9739e4076de76ffba805c085da
Richard Hughes <richard.hughes@live.com.au>
2021-08-27 03:37:34 +0000

    One error item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { merge, Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators'
 
 @Component({
@@ -19,30 +19,34 @@ export class AppComponent {
     this.formGroup = this.formBuilder.group({
       email: [null, [Validators.required, Validators.email]],
       dateRange: this.formBuilder.group({
-        start: [null, [Validators.required]],
-        end: [null, [Validators.required]]
+        start: [null],
+        end: [null]
       })
     });
 
     const emailControl = this.formGroup.controls.email;
-    this.emailError$ = emailControl.statusChanges.pipe(
+    this.emailError$ = merge(of(this.getErrorMessage(emailControl.status, emailControl)), emailControl.statusChanges.pipe(
       map(status => this.getErrorMessage(status, emailControl))
-    );
+    ));
     const dateRange = this.formGroup.controls.dateRange;
     this.dateRangeError$ = dateRange.statusChanges.pipe(
       map(status => this.getErrorMessage(status, dateRange))
     );
+    this.formGroup.updateValueAndValidity();
   }
 
   private getErrorMessage(status: string, control: AbstractControl): string | null {
     let errors!: string | null;
-    if (status === 'VALID' || control.untouched || control.errors == null) {
+    if (status === 'VALID' || control.errors == null) {
       return null;
     }
     Object.keys(control.errors).forEach(key => {
       switch (key) {
         case 'email':
           errors = 'Please enter a valid email address';
+          break;
+        case 'required':
+          errors = 'This field is required';
           break;
         case 'matStartDateInvalid':
           errors = 'The start date is invalid';
